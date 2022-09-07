7120b89f042fa9f9ad20fe9f0f1b80597570c1c0
oleg_evstifeev <evstifeev96@yandex.ru>
2022-09-07 22:26:32 +0300

    Добавили решение задачи 23 ДЗ_3

---
+++ HomeWork_C#_3/HomeWork_23_03/HomeWork_23_03.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+Console.WriteLine("Введите число N");
+    int inputNumber = int.Parse(Console.ReadLine());
+        if(inputNumber !=0)
+        {
+            for (int i = 1; i <= inputNumber; i++)
+                {
+                    if(i == 1) Console.Write($"{inputNumber} -> {Math.Pow(i, 3)},");
+                    else if (i != inputNumber) Console.Write($"{Math.Pow(i, 3)},");
+                    else Console.Write($"{Math.Pow(i, 3)}");
+                }
+        }
