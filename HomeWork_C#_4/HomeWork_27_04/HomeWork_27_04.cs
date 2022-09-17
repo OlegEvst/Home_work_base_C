f9a5b6e7976bf35cabd91a6d81c2974cba17b5f5
oleg_evstifeev <evstifeev96@yandex.ru>
2022-09-18 01:04:37 +0300

    Добавили решение задачи 27 из ДЗ4

---
+++ HomeWork_C#_4/HomeWork_27_04/HomeWork_27_04.cs
@@ -0,0 +1,33 @@
+﻿/* Задача 27: Напишите программу, которая принимает 
+на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+Console.WriteLine("Введите число: ");
+// Сообщение вводе числа
+int originalNumber = int.Parse(Console.ReadLine());
+// Проверка на целое число, если успешно, то конвертирует
+int conversionNumber = originalNumber;
+// Переменная нужна только для финального вывода в консоль
+int count = Convert.ToString(originalNumber).Length;
+// Длинну числа мы можем узнать только в строковом типе
+int MetodSum(int number)
+{
+int summ = 0;
+int result = 0;
+    for(int i = 0; i < count; i++)
+    {
+    result = originalNumber - originalNumber % 10; 
+    summ = summ + (originalNumber - result); 
+    originalNumber = originalNumber / 10; 
+    }
+    return summ;
+}
+// Метод принемает число и выполняет цикл count раз - длина числа
+// В теле цикла мы разделяем цифры числа и складываем их через вспомогательные переменные summ и result
+int printSumm = MetodSum(originalNumber);
+// Объявление метода
+Console.WriteLine($"Сумма цифр числа: '{conversionNumber}' -> '{printSumm}'");
+// Вывод результата в консоль.
+
