f82e56fcd835a93f6608f38fab6123a8c9e2b070
oleg_evstifeev <evstifeev96@yandex.ru>
2022-10-02 22:05:04 +0300

    Добавили решение 68_09

---
+++ HomeWork_C#_9/HomeWork_68_09/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана с помощью
+рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 29 */
+
+Console.WriteLine("Введите число m:");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число n:");
+int secondNumber = int.Parse(Console.ReadLine());
+
+int FunctionAkkerman(int numberOne, int numberSecond)
+{
+    if (numberOne == 0) return numberSecond + 1;
+    else if (numberOne > 0  && numberSecond == 0) return FunctionAkkerman(numberOne - 1, 1);
+    else return (FunctionAkkerman(numberOne - 1, FunctionAkkerman(numberOne, numberSecond - 1)));
+}
+
+Console.Write($"m = {firstNumber}; n = {secondNumber} -> "
+              + $"A({firstNumber},{secondNumber}) = {FunctionAkkerman(firstNumber, secondNumber)}");
